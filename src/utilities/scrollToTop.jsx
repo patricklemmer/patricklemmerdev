33d3fad0c5cda4c10e2c0b3fa5dfe61b1ac26d06
Patrick Lemmer <patrick.lemmer@yahoo.com>
2023-02-22 19:08:00 +0100

    changed position of scrollToTop button

--- src/utilities/scrollToTop.jsx
+++ src/utilities/scrollToTop.jsx
@@ -1,30 +1,30 @@
 // Imports
 // React imports
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 // Icon imports
-import { FiArrowUp } from 'react-icons/fi'
+import { FiArrowUp } from 'react-icons/fi';
 
 function ScrollToTop() {
-  const [display, setDisplay] = useState(false)
+  const [display, setDisplay] = useState(false);
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
-      setDisplay(window.scrollY > 400)
-    })
-  }, [])
+      setDisplay(window.scrollY > 400);
+    });
+  }, []);
 
   return (
     <>
       {display && (
-        <div className="flex items-center justify-center bg-skin-card-light fixed right-[5%] bottom-40 w-14 h-14 rounded-full dark:bg-skin-card-detail-dark">
+        <div className="flex items-center justify-center bg-skin-card-light fixed right-[5%] bottom-16 w-14 h-14 rounded-full drop-shadow-sm dark:bg-skin-card-detail-dark">
           <a href="#home">
             <FiArrowUp className="text-3xl opacity-80" />
           </a>
         </div>
       )}
     </>
-  )
+  );
 }
 
-export default ScrollToTop
+export default ScrollToTop;
